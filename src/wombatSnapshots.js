cb7b38879e9b257d3f5c3f067263823c69691181
Graham Kelly <gkgoat6700@gmail.com>
2025-08-12 16:54:24 +0000

    snapshots

---
+++ src/wombatSnapshots.js
@@ -0,0 +1,27 @@
+const { bind, apply: nativeApply } = Function.prototype;
+
+export const { defineProperty } = Object;
+
+/**
+ * uncurryThis()
+ * Equivalent of: fn => (thisArg, ...args) => apply(fn, thisArg, args)
+ *
+ * See those reference for a complete explanation:
+ * http://wiki.ecmascript.org/doku.php?id=conventions:safe_meta_programming
+ * which only lives at
+ * http://web.archive.org/web/20160805225710/http://wiki.ecmascript.org/doku.php?id=conventions:safe_meta_programming
+ * 
+ * Comment from https://github.com/endojs/endo/blob/master/packages/ses/src/commons.js
+ *
+ * @type {<F extends (this: any, ...args: any[]) => any>(fn: F) => ((thisArg: ThisParameterType<F>, ...args: Parameters<F>) => ReturnType<F>)}
+ */
+export const uncurryThis = bind.bind(bind.call);
+
+
+/**
+ * 
+ * @type {<F extends (this: any, ...args: any[]) => any>(fn: F, thisArg: ThisParameterType<F>, args: Parameters<F>) => ReturnType<F>}
+ */
+export const apply = uncurryThis(nativeApply);
+
+
